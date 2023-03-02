f71691e539c09db706a61f21a2212e6dd559d7ee
reza-j <23619106+reza-j@users.noreply.github.com>
2023-03-02 13:38:04 -0600

    updates to set_freq/phase/amp operations (#22)
    This PR updates set_freq/phase/amp operations
    
    ---------
    
    Co-authored-by: Marius Hillenbrand <marius.hillenbrand@ibm.com>
    Co-authored-by: Kit Barton <kit.barton@gmail.com>
    Co-authored-by: Vincent R. Pascuzzi <vrpascuzzi@ibm.com>
    Co-authored-by: mbhealy <mbhealy@ibm.com>

--- lib/Dialect/Pulse/Utils/Utils.cpp
+++ lib/Dialect/Pulse/Utils/Utils.cpp
@@ -25,23 +25,9 @@ uint getQubitId(Port_CreateOp pulsePortOp) {
   return idNum;
 }
 
-uint getQubitId(PlayOp pulsePlayOp, CallSequenceOp callSequenceOp) {
-  // get qubit id from a PlayOp under the assumption that the play op
-  // is part of a sequence
-  auto mixFrameOp = getMixFrameOp(pulsePlayOp, callSequenceOp);
+uint getQubitId(MixFrameOp mixFrameOp) {
+  // get qubit id from a MixFrameOp
   auto portOp = mixFrameOp.port().getDefiningOp<mlir::pulse::Port_CreateOp>();
-  return getQubitId(portOp);
-}
-
-uint getQubitId(CaptureOp captureOp, CallSequenceOp callSequenceOp) {
-  // get qubit id from a CaptureOp under the assumption that the capture op
-  // is part of a sequence
-  auto portOpArgIndex =
-      captureOp.target().dyn_cast<BlockArgument>().getArgNumber();
-  auto mixFrameOp = callSequenceOp.getOperand(portOpArgIndex)
-                        .getDefiningOp<mlir::pulse::MixFrameOp>();
-  auto portOp =
-      dyn_cast<mlir::pulse::Port_CreateOp>(mixFrameOp.port().getDefiningOp());
   return getQubitId(portOp);
 }
 
@@ -51,14 +37,6 @@ Waveform_CreateOp getWaveformOp(PlayOp pulsePlayOp,
   auto wfrOp = callSequenceOp.getOperand(wfrArgIndex)
                    .getDefiningOp<mlir::pulse::Waveform_CreateOp>();
   return wfrOp;
-}
-
-MixFrameOp getMixFrameOp(PlayOp pulsePlayOp, CallSequenceOp callSequenceOp) {
-  auto mixFrameArgIndex =
-      pulsePlayOp.target().dyn_cast<BlockArgument>().getArgNumber();
-  auto mixFrameOp = callSequenceOp.getOperand(mixFrameArgIndex)
-                        .getDefiningOp<mlir::pulse::MixFrameOp>();
-  return mixFrameOp;
 }
 
 bool isPlayOpForDrive(Operation *op,
