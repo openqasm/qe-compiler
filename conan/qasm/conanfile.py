b07f9e6d1759b7b247ea14041f2280aa1e12dd9b
Brian Donovan <1385694+bcdonovan@users.noreply.github.com>
2023-03-03 16:19:57 -0500

    Print source context for QASM parser errors (#59)
    This PR adds a `QasmDiagnosticEmitter` handler which uses a LLVM
    SourceMgr to add additional context to parser diagnostics.
    
    This PR has a related PR in qss-qasm
    [#12](https://github.com/Qiskit/qss-qasm/pull/12).
    
    Upgrades to QASM 0.2.13 and new recipe.
    
    ---------
    
    Co-authored-by: Marius Hillenbrand <mhillen@linux.ibm.com>
    Co-authored-by: Kevin Hartman <kevin@hart.mn>

--- conan/qasm/conanfile.py
+++ conan/qasm/conanfile.py
@@ -1,15 +1,15 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
 import os, platform
 from contextlib import contextmanager
 from conans.tools import load
 from conan.tools.apple import is_apple_os
-
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 import subprocess
 
 
 class QasmConan(ConanFile):
     name = "qasm"
-    version = "0.2.12"
+    version = "0.2.13"
     url = "https://github.com/Qiskit/qss-qasm.git"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "examples": [True, False]}
@@ -18,68 +18,46 @@ class QasmConan(ConanFile):
     author = "IBM Quantum development team"
     topics = ("Compiler", "Parser", "OpenQASM3")
     description = "Compiler for OpenQASM3 language."
-    generators = "cmake"
 
     def source(self):
-        self.run(f"git clone git@github.com:Qiskit/qss-qasm.git")
+        token = os.environ.get("GITHUB_PAT")
+        if token is not None:
+            self.run(f"git clone https://{token}@github.com/Qiskit/qss-qasm.git .")
+        else:
+            self.run(f"git clone git@github.com:Qiskit/qss-qasm.git .")
 
         commit_hash = self.conan_data["sources"]["hash"]
-        self.run(f"cd qss-qasm && git checkout {commit_hash} && cd ..")
-
-        # When building on Apple, we need to pass the KEEP_RPATHS option to the
-        # basic setup command for Conan.
-        keep_rpaths = "KEEP_RPATHS" if tools.is_apple_os(self.settings.os) else ""
-
-        tools.replace_in_file(
-            "qss-qasm/CMakeLists.txt",
-            """project(OPENQASM VERSION "${OPENQASM_VERSION_TRIPLE}")""",
-            """project(OPENQASM VERSION "${OPENQASM_VERSION_TRIPLE}")
-        if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-            message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-            file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/master/conan.cmake"
-                "${CMAKE_BINARY_DIR}/conan.cmake")
-        endif()
-        include(${CMAKE_BINARY_DIR}/conan.cmake)
-        conan_cmake_run(CONANFILE ${CMAKE_SOURCE_DIR}/conanfile.py
-            BASIC_SETUP """
-            + keep_rpaths
-            + """
-            BUILD missing)
-            """,
-        )
-
-        tools.replace_in_file(
-            "qss-qasm/lib/Parser/CMakeLists.txt",
-            """set(BISON_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/QasmParser.tab.cpp")""",
-            """set(SETUP_M4 ${CMAKE_COMMAND} -E env M4=${CONAN_BIN_DIRS_M4}/m4)
-        set(BISON_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/QasmParser.tab.cpp")
-        """,
-        )
-
-        tools.replace_in_file(
-            "qss-qasm/lib/Parser/CMakeLists.txt",
-            """COMMAND ${BISON_EXECUTABLE}""",
-            """COMMAND ${SETUP_M4} ${BISON_EXECUTABLE}
-        """,
-        )
+        self.run(f"git checkout {commit_hash}")
 
     def requirements(self):
+        # Private deps won't be linked against by consumers, which is important
+        # at least for Flex which does not expose a CMake target.
+        private_deps = ["bison", "flex"]
         for req in self.conan_data["requirements"]:
-            self.requires(req)
+            private = any(req.startswith(d) for d in private_deps)
+            self.requires(req, private=private)
 
-    def _configure_cmake(self):
-        cmake = CMake(self, parallel=False)
-        cmake.verbose = True
-        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-        cmake.definitions["BUILD_STATIC_LIBS"] = not self.options.shared
-        cmake.definitions["OPENQASM_BUILD_EXAMPLES"] = self.options.examples
-        cmake.definitions["CMAKE_BUILD_TYPE"] = self.settings.build_type
+    def build_requirements(self):
+        for req in self.conan_data["build_requirements"]:
+            self.tool_requires(req)
 
-        return cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.cache_variables["BUILD_STATIC_LIBS"] = not self.options.shared
+        tc.cache_variables["OPENQASM_BUILD_EXAMPLES"] = self.options.examples
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.configure(source_folder="qss-qasm")
+        cmake = CMake(self)
+        cmake.configure()
+
         # Note that if a job does not produce output for a longer period of
         # time, then Travis will cancel that job.
         # Avoid that timeout by running vmstat in the background, which reports
@@ -94,10 +72,14 @@ class QasmConan(ConanFile):
             monitor.wait(1)
 
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
         self.copy("*.tab.cpp", dst="include/lib/Parser", src="lib/Parser")
         self.copy("*.tab.h", dst="include/lib/Parser", src="lib/Parser")
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_file_name", "qasm")
+        self.cpp_info.set_property("cmake_target_name", "qasm::qasm")
+
         self.cpp_info.libs = ["qasmParser", "qasmFrontend", "qasmAST", "qasmDIAG"]
