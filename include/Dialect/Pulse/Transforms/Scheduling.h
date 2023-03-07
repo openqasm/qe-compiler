71fe4c62d8609f938ce435c2fdb6a84ff4eb5353
Brian Donovan <brian.donovan@ibm.com>
2023-03-06 19:01:28 -0500

    add qiskit / apache license - Pulse includes

--- include/Dialect/Pulse/Transforms/Scheduling.h
+++ include/Dialect/Pulse/Transforms/Scheduling.h
@@ -1,17 +1,26 @@
-////===- Scheduling.h - Add absolute timing to defcal calls. ------*- C++
-///-*-===//
-////
-//// (C) Copyright IBM 2021.
-////
-//// Any modifications or derivative works of this code must retain this
-//// copyright notice, and modified files need to carry a notice indicating
-//// that they have been altered from the originals.
-////
-////===----------------------------------------------------------------------===//
-////
-////  This file declares the pass for adding absolute timing to defcal calls.
-////
-////===----------------------------------------------------------------------===//
+//===- Scheduling.h - Add absolute timing to defcal calls. ------*- C++ -*-===//
+//
+// (C) Copyright IBM 2021, 2023.
+//
+// This code is part of Qiskit.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file declares the pass for adding absolute timing to defcal calls.
+//
+//===----------------------------------------------------------------------===//
 
 //#ifndef PULSE_SCHEDULING_H
 //#define PULSE_SCHEDULING_H
