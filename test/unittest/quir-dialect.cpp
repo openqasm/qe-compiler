87f0b3139468c7f89fb04e93fa63c7570354ccac
Brian Donovan <brian.donovan@ibm.com>
2023-03-06 20:15:38 -0500

    add qiskit / apache license

--- test/unittest/quir-dialect.cpp
+++ test/unittest/quir-dialect.cpp
@@ -1,10 +1,20 @@
 //===- quir-dialect.cpp - Unit tests for quir dialect -----------*- C++ -*-===//
 //
-// (C) Copyright IBM 2021, 2022.
+// (C) Copyright IBM 2021 - 2023.
 //
-// Any modifications or derivative works of this code must retain this
-// copyright notice, and modified files need to carry a notice indicating
-// that they have been altered from the originals.
+// This code is part of Qiskit.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 //===----------------------------------------------------------------------===//
 
