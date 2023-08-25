12c303b64b6a84e72ad9b6071632a0c920e688b9
Joshua Lou <joshualou@joshuas-mbp.watson.ibm.com>
2023-08-25 10:22:37 -0400

    fix oq3 and qcs names p2

---
+++ python_lib/qss_compiler/mlir/dialects/oq3.py
@@ -0,0 +1,7 @@
+from ._oq3_ops_gen import *  # noqa: F403, F401
+from .._mlir_libs._ibmDialectsOQ3 import *  # noqa: F403, F401
+
+from ..ir import UnitAttr
+from . import scf
+
+
---
+++ python_lib/qss_compiler/mlir/dialects/qcs.py
@@ -0,0 +1,7 @@
+from ._qcs_ops_gen import *  # noqa: F403, F401
+from .._mlir_libs._ibmDialectsQCS import *  # noqa: F403, F401
+
+from ..ir import UnitAttr
+from . import scf
+
+
