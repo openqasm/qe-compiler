947e2b2b09a6eca962951d59d31dfa14c3e3c315
Blake Johnson <blake.johnson@ibm.com>
2023-12-21 14:01:02 -0500

    Update qss-qasm reference to qe-qasm in conan.

--- conan/qasm/conanfile.py
+++ conan/qasm/conanfile.py
@@ -21,7 +21,7 @@ import subprocess
 class QasmConan(ConanFile):
     name = "qasm"
     version = "0.2.16"
-    url = "https://github.com/Qiskit/qss-qasm.git"
+    url = "https://github.com/Qiskit/qe-qasm.git"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "examples": [True, False]}
     default_options = {
@@ -39,9 +39,9 @@ class QasmConan(ConanFile):
     def source(self):
         token = os.environ.get("GITHUB_PAT")
         if token is not None:
-            self.run(f"git clone https://{token}@github.com/Qiskit/qss-qasm.git .")
+            self.run(f"git clone https://{token}@github.com/Qiskit/qe-qasm.git .")
         else:
-            self.run(f"git clone git@github.com:Qiskit/qss-qasm.git .")
+            self.run(f"git clone git@github.com:Qiskit/qe-qasm.git .")
 
         commit_hash = self.conan_data["sources"]["hash"]
         self.run(f"git checkout {commit_hash}")
