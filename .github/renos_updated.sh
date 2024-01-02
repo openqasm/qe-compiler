99452b9c2cef9eec5c98546e0225e2c376a8795a
Thomas Alexander <thomas.a.alexander@ibm.com>
2024-01-02 18:21:21 -0400

    Fix branch match for github ci.

--- .github/renos_updated.sh
+++ .github/renos_updated.sh
@@ -3,12 +3,9 @@
 
 reno lint
 
-CHANGED_FILES=$(git diff --name-only $BRANCH HEAD)
-echo $CHANGED_FILES
+CHANGED_FILES=$(git diff --name-only $BRANCH $TARGET_BRANCH_NAME)
 for file in $CHANGED_FILES
 do
-   echo "Debug output:"
-   echo "./$file" | awk -F/ '{print FS $2}' | cut -c2-
    root=$(echo "./$file" | awk -F/ '{print FS $2}' | cut -c2-)
    if [ "$root" = "releasenotes" ]; then
        exit 0;
