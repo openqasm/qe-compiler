71e685195232686b6cd9b3634d78cde4fb89b46a
Michael Healy <mbhealy@ibm.com>
2024-02-22 09:54:32 -0500

    Missed _qcs_ops_ext.py

--- python_lib/qss_compiler/mlir/dialects/_qcs_ops_ext.py
+++ python_lib/qss_compiler/mlir/dialects/_qcs_ops_ext.py
@@ -1,10 +1,13 @@
 try:
-    from ..ir import *
-    from .qcs import *
-    from .._mlir_libs._ibmDialectsQCS import *
-    from ._ods_common import get_default_loc_context as _get_default_loc_context
+    from ..ir import *  # noqa: F401, F403
+    from ..ir import IntegerAttr, IntegerType, BoolAttr
+    from .oq3 import *  # noqa: F401, F403
+    from .._mlir_libs._ibmDialectsOQ3 import *  # noqa: F401, F403
+    from ._ods_common import (  # noqa: F401
+        get_default_loc_context as _get_default_loc_context,
+    )
 
-    from typing import Any, List, Optional, Union
+    from typing import Any, List, Optional, Union  # noqa: F401
 except ImportError as e:
     raise RuntimeError("Error loading imports from extension module") from e
 
@@ -12,5 +15,7 @@ except ImportError as e:
 class ShotInitOp:
     def __init__(self, numShots, *, startNCOs=False, loc=None, ip=None):
         super().__init__(loc=loc, ip=ip)
-        self.attributes["qcs.numShots"] = IntegerAttr.get(IntegerType.get_signless(32), numShots)
+        self.attributes["qcs.numShots"] = IntegerAttr.get(
+            IntegerType.get_signless(32), numShots
+        )  # black/flake8 line length conflict
         self.attributes["quir.startNCOs"] = BoolAttr.get(startNCOs)
