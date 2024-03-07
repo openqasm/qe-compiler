cdea0889b0c99c0bc25645b384e874c57ad19f6f
Brian Donovan <1385694+bcdonovan@users.noreply.github.com>
2024-03-07 11:52:02 -0500

    Update lib/Dialect/QUIR/Transforms/ExtractCircuits.cpp
    Co-authored-by: mbhealy <mbhealy83@gmail.com>

--- lib/Dialect/QUIR/Transforms/ExtractCircuits.cpp
+++ lib/Dialect/QUIR/Transforms/ExtractCircuits.cpp
@@ -29,7 +29,6 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/Attributes.h"
-
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/IRMapping.h"
 #include "mlir/IR/Location.h"
