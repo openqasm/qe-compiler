cb856f01e92387baef6f040d5969fba510b8e2ab
reza-j <23619106+reza-j@users.noreply.github.com>
2024-03-27 18:07:59 -0500

    Update Scheduling.h copyright year

--- include/Dialect/Pulse/Transforms/Scheduling.h
+++ include/Dialect/Pulse/Transforms/Scheduling.h
@@ -1,6 +1,6 @@
 //===- scheduling.h --- quantum circuits pulse scheduling -------*- C++ -*-===//
 //
-// (C) Copyright IBM 2023.
+// (C) Copyright IBM 2023, 2024.
 //
 // This code is part of Qiskit.
 //
